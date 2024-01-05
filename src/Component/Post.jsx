af94e116cab639496bd52f630f5abd3949a84fc0
u-siop <jinwook.jun5@gmail.com>
2024-01-05 21:06:46 +0100

    css and component module css

--- src/Component/Post.jsx
+++ src/Component/Post.jsx
@@ -1,11 +1,13 @@
+import classes from './Post.module.css';
+
 // const names = ['Maximilian', 'Manuel'];
 function Post(props){
     // const chosenName = Math.random() > 0.5 ? names[0] : names[1];
 
     return(
-        <div>
-            <p>{props.author}</p>
-            <p>{props.body}</p>
+        <div className={classes.post}>
+            <p className={classes.author}>{props.author}</p>
+            <p className={classes.text}>{props.body}</p>
         </div>
     );
 }
