7e17eea39f23bf5a5e16dcb53fb61723a076b5cf
u-siop <jinwook.jun5@gmail.com>
2024-01-06 13:24:39 +0100

    feat: about the state

--- src/Component/Post.jsx
+++ src/Component/Post.jsx
@@ -1,9 +1,6 @@
 import classes from './Post.module.css';
 
-// const names = ['Maximilian', 'Manuel'];
 function Post(props){
-    // const chosenName = Math.random() > 0.5 ? names[0] : names[1];
-
     return(
         <li className={classes.post}>
             <p className={classes.author}>{props.author}</p>
--- src/Component/PostsList.jsx
+++ src/Component/PostsList.jsx
@@ -1,12 +1,27 @@
+import { useState } from 'react';
 import Post from './Post';
+import NewPost from './NewPost';
 import classes from './PostsList.module.css'
 
 function PostsList(){
+    const [ enterBody, setEnteredBody ] = useState('');
+    const [ enterAuthor, setEnteredAuthor ] = useState('');
+
+    function bodyChangeHandler(event){
+        setEnteredBody(event.target.value);
+    }
+    function authorChangeHandler(event){
+        setEnteredAuthor(event.target.value);
+    }
+
     return(
-        <ul className={classes.posts}>
-            <Post author="Maxmilian" body="React.js is Awesome!"/>
-            <Post author="Manuel" body="Check out the every course"/>
-        </ul>
+        <>
+            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
+            <ul className={classes.posts}>
+                <Post author={enterAuthor} body={enterBody}/>
+                <Post author="Manuel" body="Check out the every course"/>
+            </ul>
+        </>
     );
 }
 
