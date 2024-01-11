5f69528fc72ef284a9df3fd10b86a47bf1f18b5b
u-siop <jinwook.jun5@gmail.com>
2024-01-11 10:23:52 +0100

    feat: route layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Router, RouterProvider, createBrowserRouter } from 'react-router-dom'
+
 import App from './App'
+import NewPost from './Component/NewPost'
 import './index.css'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/create-post', element: <NewPost /> }
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 )
